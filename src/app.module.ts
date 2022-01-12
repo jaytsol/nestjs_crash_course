a3663f101eef7ad2729e80b76f5d72992980efe9
Jay <jaewhee@tictag.io>
2022-01-12 16:09:59 +0900

    error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
-import { ItemsModule } from './items/items.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ItemsModule, MongooseModule.forRoot()],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://jaytsol:poiu8797!@nestcluster.8eug4.mongodb.net/test',
+};

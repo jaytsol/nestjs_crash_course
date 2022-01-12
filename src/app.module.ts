c7f15a81ef7e2ef39ba02edcef16c8caa62372b7
Jay <jaewhee@tictag.io>
2022-01-12 17:05:49 +0900

    add ItemsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './config/keys';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
